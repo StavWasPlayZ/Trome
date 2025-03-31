ab59303a9134828c1422d3f796b0ce102642574e
StavWasPlayZ <stavsolomon101@gmail.com>
2025-03-30 23:54:01 -0700

    Separate Server logic to Communicator

---
+++ Server/src/infrastructure/Communicator.cpp
@@ -0,0 +1,181 @@
+#include "Communicator.h"
+
+#include "../exception/WSAException.h"
+
+#include <iostream>
+
+const std::string Communicator::CMD_HELLO = "Hello";
+
+Communicator::Communicator() :
+    _serverSocket(INVALID_SOCKET),
+    _address({ 0 }),
+    _running(false)
+{
+}
+
+Communicator::~Communicator()
+{
+    close();
+}
+
+void Communicator::close()
+{
+    if (!this->_running)
+        return;
+
+    // Notify all threads that the server is closing
+    this->_running = false;
+
+    // Wait for 'em to close
+    for (std::future<void>& future : this->_clientThreads)
+    {
+        future.wait();
+    }
+
+    if (this->_serverSocket != INVALID_SOCKET)
+    {
+        closesocket(this->_serverSocket);
+    }
+
+    WSACleanup();
+
+    this->_serverSocket = INVALID_SOCKET;
+    this->_address = { 0 };
+
+    this->_serverThread = std::future<void>();
+}
+
+bool Communicator::isRunning() const
+{
+    return this->_running;
+}
+
+std::future<void>& Communicator::bindAndListen()
+{
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        throw WSAException("WSAStartup failed");
+    }
+
+    // Create socket file descriptor
+    this->_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (this->_serverSocket == INVALID_SOCKET)
+    {
+        close();
+        throw WSAException("Socket creation failed");
+    }
+
+    // The timeout for the recv method.
+    // Set in place to allow refreshing the value of _running.
+    setsockopt(this->_serverSocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&RECV_REFRESH_TIMEOUT, sizeof(RECV_REFRESH_TIMEOUT));
+
+    // Bind the socket to the port
+    _address.sin_family = AF_INET;
+    _address.sin_addr.s_addr = INADDR_ANY;
+    _address.sin_port = htons(PORT);
+
+    if (bind(this->_serverSocket, (struct sockaddr*)&_address, sizeof(_address)) == SOCKET_ERROR)
+    {
+        close();
+        throw WSAException("Bind failed");
+    }
+
+    this->_running = true;
+
+    // Start listening for connections
+    if (listen(this->_serverSocket, 3) == SOCKET_ERROR)
+    {
+        close();
+        throw WSAException("Listen failed");
+    }
+
+    std::cout << "Listening on port " << PORT << "..." << std::endl;
+
+    return this->_serverThread = std::async(
+        std::launch::async,
+        [this]()
+        {
+            _acceptClients();
+        }
+    );
+}
+
+void Communicator::_acceptClients()
+{
+    int addrLen = sizeof(this->_address);
+
+    while (this->_running)
+    {
+        const SOCKET newSocket = accept(this->_serverSocket, (struct sockaddr*)&_address, &addrLen);
+
+        if (newSocket == INVALID_SOCKET)
+        {
+            throw WSAException("Accept failed");
+        }
+
+        std::cout << "Connection accepted" << std::endl;
+
+        this->_clientThreads.push_back(std::async(
+            std::launch::async,
+            [this, newSocket]()
+            {
+                _handleClient(newSocket);
+            }
+        ));
+    }
+
+    close();
+}
+
+void Communicator::_handleClient(const SOCKET socket) const
+{
+    sendMsg(socket, CMD_HELLO.c_str(), CMD_HELLO.length(), 0);
+
+    while (this->_running)
+    {
+        char buffer[6];
+        if (!recieveMsg(socket, buffer, sizeof(buffer), 0))
+        {
+            // If we timed out (see RECV_REFRESH_TIMEOUT),
+            // simply wait for the next recv cycle (if applicable).
+            continue;
+        }
+
+        buffer[5] = 0;
+
+        if (buffer == CMD_HELLO)
+        {
+            sendMsg(socket, CMD_HELLO.c_str(), CMD_HELLO.length(), 0);
+        }
+    }
+
+    closesocket(socket);
+}
+
+bool Communicator::recieveMsg(const SOCKET socket, char* buffer, const int length, const int flags) const
+{
+    int result = recv(socket, buffer, length, flags);
+
+    if (result == SOCKET_ERROR)
+    {
+        if (WSAGetLastError() == WSAETIMEDOUT)
+        {
+            return false;
+        }
+
+        throw WSAException("Error occured while handling client socket " + std::to_string(socket));
+    }
+
+    return true;
+}
+
+void Communicator::sendMsg(const SOCKET socket, const char* buffer, const int length, const int flags) const
+{
+    int sent = send(socket, buffer, length, flags);
+
+    if (sent == -1)
+    {
+        throw WSAException("Failed to send message to client socket " + std::to_string(socket));
+    }
+}
---
+++ Server/src/infrastructure/Communicator.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <string>
+
+// Much (much) better than threads in modern C++, and this usecase in particular.
+#include <future>
+// It was suggested online to use this when sharing a resource.
+#include <atomic>
+
+class Communicator
+{
+public:
+	Communicator();
+	~Communicator();
+
+	/**
+	* Binds this instance to the program port, and begins to listen for new clients.
+	*
+	* Returns: The future handling the client sockets.
+	* Completes when server closes.
+	*/
+	std::future<void>& bindAndListen();
+	void close();
+
+	bool isRunning() const;
+
+private:
+	static constexpr unsigned int PORT = 6942;
+	static const std::string CMD_HELLO;
+
+	/**
+	* The timeout for the recv method.
+	* Set in place to allow refreshing the value of _running.
+	*/
+	static constexpr unsigned int RECV_REFRESH_TIMEOUT = 3000;
+
+	SOCKET _serverSocket;
+	struct sockaddr_in _address;
+
+	std::atomic<bool> _running;
+	std::future<void> _serverThread;
+	std::vector<std::future<void>> _clientThreads;
+
+
+	void _acceptClients();
+	void _handleClient(const SOCKET socket) const;
+
+	/**
+	* Returns true whether the message did not time out.
+	*/
+	bool recieveMsg(const SOCKET socket, char* buffer, const int length, const int flags) const;
+	void sendMsg(const SOCKET socket, const char* buffer, const int length, const int flags) const;
+};
