02d1cee86c77f4e306f253f8178c270b1191bb88
StavWasPlayZ <stavsolomon101@gmail.com>
2025-03-30 21:01:09 -0700

    Convert Server file to class

--- Server/src/Server.cpp
+++ Server/src/Server.cpp
@@ -1,76 +1,91 @@
-﻿#include <iostream>
+﻿#include "Server.h"
 
 #include <iostream>
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include <exception>
 
 #pragma comment(lib, "ws2_32.lib")
 
-constexpr unsigned int PORT = 6942;
-const std::string CMD_EXIT = "EXIT";
-const std::string CMD_HELLO = "Hello";
+const std::string Server::CMD_EXIT = "EXIT";
+const std::string Server::CMD_HELLO = "Hello";
 
-int main()
+Server::Server() :
+    _serverSocket(INVALID_SOCKET),
+    _address({0})
+{}
+
+Server::~Server()
+{
+    close();
+}
+
+void Server::close()
+{
+    if (this->_serverSocket != INVALID_SOCKET)
+    {
+        closesocket(this->_serverSocket);
+    }
+
+    WSACleanup();
+
+    this->_serverSocket = INVALID_SOCKET;
+    this->_address = { 0 };
+}
+
+void Server::run()
 {
     WSADATA wsaData;
-    SOCKET server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-
-    // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        std::cerr << "WSAStartup failed" << std::endl;
-        return 1;
+        throw std::exception("WSAStartup failed");
     }
 
     // Create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-{
-        std::cerr << "Socket creation failed" << std::endl;
-        WSACleanup();
-        return 1;
+    if ((this->_serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        close();
+        throw std::exception("Socket creation failed");
     }
 
     // Bind the socket to the port
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    _address.sin_family = AF_INET;
+    _address.sin_addr.s_addr = INADDR_ANY;
+    _address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR)
+    if (bind(this->_serverSocket, (struct sockaddr*)&_address, sizeof(_address)) == SOCKET_ERROR)
 {
-        std::cerr << "Bind failed" << std::endl;
-        closesocket(server_fd);
-        WSACleanup();
-        return 1;
+        close();
+        throw std::exception("Bind failed");
     }
 
     // Start listening for connections
-    if (listen(server_fd, 3) == SOCKET_ERROR)
+    if (listen(this->_serverSocket, 3) == SOCKET_ERROR)
 {
-        std::cerr << "Listen failed" << std::endl;
-        closesocket(server_fd);
-        WSACleanup();
-        return 1;
+        close();
+        throw std::exception("Listen failed");
     }
 
     std::cout << "Listening on port " << PORT << "..." << std::endl;
 
     // Accept incoming connections
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) == INVALID_SOCKET)
-{
-        std::cerr << "Accept failed" << std::endl;
-        closesocket(server_fd);
-        WSACleanup();
-        return 1;
-    }
+    _acceptClients();
 
     std::cout << "Connection accepted" << std::endl;
+    close();
+}
 
-    // Close sockets
-    closesocket(new_socket);
-    closesocket(server_fd);
-    WSACleanup();
+void Server::_acceptClients() const
+{
+    int addrLen = sizeof(this->_address);
 
-	return 0;
+    //while (true)
+    {
+        SOCKET new_socket;
+
+        if ((new_socket = accept(this->_serverSocket, (struct sockaddr*)&_address, &addrLen)) == INVALID_SOCKET)
+        {
+            throw std::exception("Accept failed");
+            closesocket(this->_serverSocket);
+            WSACleanup();
+        }
+    }
 }
---
+++ Server/src/Server.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <string>
+
+class Server
+{
+public:
+	Server();
+	~Server();
+
+	void run();
+	void close();
+
+private:
+	static constexpr unsigned int PORT = 6942;
+	static const std::string CMD_EXIT;
+	static const std::string CMD_HELLO;
+
+	SOCKET _serverSocket;
+	struct sockaddr_in _address;
+
+	void _acceptClients() const;
+};
