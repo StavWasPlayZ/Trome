065b02fb7efca80702edaf10559eb79751110f92
Itay-He <hershkoitai@gmail.com>
2025-04-01 18:09:24 +0300

    Made the signup and error serializer

--- Server/src/common/infrastructure/JsonResponsePacketSerializer.hpp
+++ Server/src/common/infrastructure/JsonResponsePacketSerializer.hpp
@@ -34,13 +34,41 @@ public:
 		Note: this function makes a new unsigned char*
 		after using the buffer delete it
 	*/
-	static unsigned char* serializeSignupResponse(const SignupResponse& response);
+	static unsigned char* serializeSignupResponse(const SignupResponse& response)
+	{
+		json j = { {STATUS_KEY, response.status} };
+		std::string str = j.dump();
+
+		int len = CODE_SIZE + JSON_LEN_SIZE + str.size() + 1; // + 1 for null terminator
+		unsigned char* buffer = new unsigned char[len];
+
+		buffer[0] = SIGNUP_CODE;
+		JsonResponsePacketSerializer::writeIntToFourBytes(str.size() + 1, buffer);  // + 1 for null terminator
+
+		memcpy(buffer, str.c_str(), len);
+
+		return buffer;
+	}
 
 	/*
 		Note: this function makes a new unsigned char*
 		after using the buffer delete it
 	*/
-	static unsigned char* serializeErrorResponse(const ErrorResponse& response);
+	static unsigned char* serializeErrorResponse(const ErrorResponse& response)
+	{
+		json j = { {MESSAGE_KEY, response.message} };
+		std::string str = j.dump();
+
+		int len = CODE_SIZE + JSON_LEN_SIZE + str.size() + 1; // + 1 for null terminator
+		unsigned char* buffer = new unsigned char[len];
+
+		buffer[0] = ERROR_CODE;
+		JsonResponsePacketSerializer::writeIntToFourBytes(str.size() + 1, buffer);  // + 1 for null terminator
+
+		memcpy(buffer, str.c_str(), len);
+
+		return buffer;
+	}
 
 private:
 	static void writeIntToFourBytes(int num, unsigned char* buffer)
