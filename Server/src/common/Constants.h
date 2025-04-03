2b68558ede9bb53068dc38cc633a11d14b211267
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-03 11:14:52 +0300

    Re-fix incorrect sizes

--- Server/src/common/Constants.h
+++ Server/src/common/Constants.h
@@ -16,5 +16,5 @@ struct ProtocolJsonKeys
 };
 
 // sizes
-constexpr unsigned int SIZE_CODE = sizeof(int);
+constexpr unsigned int SIZE_CODE = sizeof(char);
 constexpr unsigned int SIZE_JSON_LEN = sizeof(int);
