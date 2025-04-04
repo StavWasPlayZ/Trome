72d716eaa900ff444d873d33e617cf63b1ee0320
Itay-He <hershkoitai@gmail.com>
2025-04-04 15:25:30 +0300

    Made python serialzer

--- PythonTest/test101.py
+++ PythonTest/test101.py
@@ -1,25 +1,45 @@
 import socket
+import json
+
+LOGIN = 1
+SIGNUP = 2
+
+JSON_TEST_LOGIN = {"username": "user1", "password": "1234"}
+JSON_TEST_SIGNUP = {"username": "user1", "password": "1234", "mail": "user1@gmail.com"}
 
 SERVER_INFO = ("127.0.0.1", 6942)
-CMD_HELLO = "Hello"
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(SERVER_INFO)
 
-        while True:
-            try:
-                server_msg = sock.recv(1024).decode()
-            except:
-                # Socket was probably just closed.
-                break
-            
+        try:
+            msg = serialize(LOGIN, JSON_TEST_LOGIN)
+            sock.sendall(msg)
+            print("Sent: Code:", LOGIN, "\nData:", JSON_TEST_LOGIN, "\nIn bytes:", msg)
+
+            server_msg = sock.recv(1024).decode()
+        
             print("Recieved:", server_msg)
 
-            if server_msg == CMD_HELLO:
-                sock.sendall(CMD_HELLO.encode())
-                print("Sent:", CMD_HELLO)
+        except:
+            print("Something went wrong :(")
+            return
 
+def serialize(code, data):
+    """
+    serialze data
+    :param code: code to send
+    :param data: data to send
+    :type code: int
+    :type data: json
+    :return: bytes to send
+    :rtype: bytes
+    """
+    json_bytes = json.dumps(data).encode('utf-8')
+    json_len_bytes = len(json_bytes).to_bytes(4, byteorder='big') # big means the format we need
+    message = bytes([code]) + json_len_bytes + json_bytes
+    return message
 
 if __name__ == "__main__":
     main()
