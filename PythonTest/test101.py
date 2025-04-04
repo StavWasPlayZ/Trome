ef8546bf263ec660055553a0485af8745a1ad8cd
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-04 16:07:54 +0300

    Reform Python documentation; Move type declarations to function signatures

--- PythonTest/test101.py
+++ PythonTest/test101.py
@@ -1,6 +1,8 @@
 import socket
 import json
 
+from typing import *
+
 LOGIN = 1
 SIGNUP = 2
 
@@ -20,57 +22,47 @@ def main():
             print("Something went wrong :(")
             return
         
-def sendAndRecv(code, data_to_send, sock):
+def sendAndRecv(code: int, data: json, sock: socket.socket):
     """
-    sends then data and recv server's response
-    :param data_to_send: data to send
-    :param code: code to send
-    :param sock: socket
-    :type data: json
-    :type code: int
-    :type sock: socket
-    :return: none
-    :rtype: none
+    Sends the provided data, and fetches the server's response
+    :param data_to_send: The data to send
+    :param code: The code to send
+    :param sock: The server socket
     """
-    msg = serialize(code, data_to_send)
+    msg = serialize(code, data)
     sock.sendall(msg)
-    print("Sent: Code:", code, "\nData:", JSON_TEST_LOGIN, "\nIn bytes:", msg)
+    print("Sent: Code:", code, "\nData:", data, "\nIn bytes:", msg)
 
     server_msg = sock.recv(1024)
-    data = deserialzer(server_msg)
+    raw_data = deserialzer(server_msg)
 
-    print("Sent: Code:", data[0], "\nData Len:", data[1], "\nData:", data[2], "\nIn bytes:", server_msg)
-    return
+    print("Sent: Code:", raw_data[0], "\nData Len:", raw_data[1], "\nData:", raw_data[2], "\nIn bytes:", server_msg)
 
 
-def serialize(code, data):
+def serialize(code: int, data: json) -> bytes:
     """
-    serialze data
-    :param code: code to send
-    :param data: data to send
-    :type code: int
-    :type data: json
+    Serialzes the provided data
+    :param code: The code to send
+    :param data: The data to send
     :return: bytes to send
-    :rtype: bytes
     """
     json_bytes = json.dumps(data).encode('utf-8')
     json_len_bytes = len(json_bytes).to_bytes(4, byteorder='big') # big is the format we need
     message = bytes([code]) + json_len_bytes + json_bytes
+
     return message
 
-def deserialzer(data):
+def deserialzer(data: bytes) -> Tuple[int, int, Any]:
     """
-    deserialze data
-    :param data: data to send
-    :type data: bytes
-    :return: code, json_len, json
-    :rtype: tuple
+    Deserialzes the provided data
+    :param data: The data to send
     """
     code = data[0]
     json_len = int.from_bytes(data[1:5], byteorder='big')  # big is the format we need
     json_bytes = data[5:5+json_len]
     data_json = json.loads(json_bytes.decode('utf-8'))
-    return code, json_len, data_json
+
+    return (code, json_len, data_json)
 
 
 if __name__ == "__main__":
