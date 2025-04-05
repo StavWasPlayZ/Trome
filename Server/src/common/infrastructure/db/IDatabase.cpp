3055f4970865dd2a0c1d5c5dd9ba898b63e590e4
Itay-He <hershkoitai@gmail.com>
2025-04-05 14:33:31 +0300

    Made IDatabase

---
+++ Server/src/common/infrastructure/db/IDatabase.cpp
@@ -0,0 +1,7 @@
+#include "IDatabase.h"
+
+IDatabase::IDatabase(const std::string dbName) : _dbName(dbName)
+{
+}
+
+IDatabase::~IDatabase() = default;
--- Server/src/common/infrastructure/db/IDatabase.h
+++ Server/src/common/infrastructure/db/IDatabase.h
@@ -1,6 +1,18 @@
 #pragma once
+#include <iostream>
 
 class IDatabase
 {
+public:
+	IDatabase(const std::string dbName);
+	virtual ~IDatabase();
 
+	virtual bool open() const = 0;
+	virtual bool close() const = 0;
+	virtual bool doesUserExists(const std::string username) const = 0;
+	virtual bool doesPasswordMatch(const std::string username, const std::string password) const = 0;
+	virtual bool addNewUser(const std::string username, const std::string password, const std::string mail) const = 0;
+
+private:
+	std::string _dbName;
 };
