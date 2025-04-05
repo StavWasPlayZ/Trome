37cca3e2523aca55057a5339f3f18433e1913548
Itay-He <hershkoitai@gmail.com>
2025-04-05 19:54:09 +0300

    Made login

--- Server/src/common/manager/LoginManager.cpp
+++ Server/src/common/manager/LoginManager.cpp
@@ -5,11 +5,22 @@ unsigned char LoginManager::signup(const std::string& username, const std::strin
 	try
 	{
 		this->m_database->addNewUser(username, password, mail);
-		this->m_loggedUsers.push_back(username);
-		return (unsigned char)ProtocolCode::OK;
+		return this->login(username, password);
 	}
 	catch (std::runtime_error& e) // addNewUser will return runtime_error when adding a user with the same username bc its UNIQE
 	{
 		return (unsigned char)ProtocolCode::ERROR;
 	}
+}
+
+
+unsigned char LoginManager::login(const std::string& username, const std::string& password)
+{
+	if (!(this->m_database->doesPasswordMatch(username, password)))
+	{
+		return (unsigned char)ProtocolCode::ERROR;
+	}
+
+	this->m_loggedUsers.push_back(username);
+	return (unsigned char)ProtocolCode::OK;
 }
