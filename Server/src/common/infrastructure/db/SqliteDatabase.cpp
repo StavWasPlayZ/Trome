e7c75cc1a3471417d972295507100df367e1139e
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-15 15:13:26 +0300

    Fix phone length

--- Server/src/common/infrastructure/db/SqliteDatabase.cpp
+++ Server/src/common/infrastructure/db/SqliteDatabase.cpp
@@ -1,6 +1,7 @@
 #include "SqliteDatabase.h"
 
 #include <sstream>
+#include <iostream>
 
 const std::string SqliteDatabase::TABLE_USERS = "users";
 
@@ -12,7 +13,7 @@ const std::string SqliteDatabase::CREATE_USERS_TBL_QUERY =
 		"password NVARCHAR(64) NOT NULL, "
 		"email NVARCHAR(254) NOT NULL, "
 
-		"phone NVARCHAR(180), "
+		"phone NVARCHAR(16), "
 		"address NVARCHAR(180), "
 		// Would 99% of times be of type DATE,
 		// but shall be entertained as a string for the sake of the exercise.
