dbc6b5de5b202e74646f5833a8d40ca6b3d3024b
Itay-He <hershkoitai@gmail.com>
2025-04-16 18:17:59 +0300

    Added struct and enum that contains the data

--- Server/src/common/infrastructure/Room.h
+++ Server/src/common/infrastructure/Room.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <string>
+
+enum class RoomStatus : unsigned int
+{
+    UNACTIVE = 0,
+    ACTIVE = 1
+};
+
+struct RoomData
+{
+    unsigned int id;
+    std::string name;
+    unsigned int maxPlayers;
+    unsigned int timePerQuestion;
+    RoomStatus status;
+};
