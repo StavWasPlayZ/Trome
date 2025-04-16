83dd3c680221331a02423e2b6cebc95d25a460a0
Itay-He <hershkoitai@gmail.com>
2025-04-16 18:46:08 +0300

    Made RoomManager.h

--- Server/src/common/manager/RoomManager.h
+++ Server/src/common/manager/RoomManager.h
@@ -1,1 +1,22 @@
 # pragma once
+
+#include <map>
+#include <optional>
+#include "infrastructure/Room.h"
+
+class RoomManager
+{
+private:
+    std::map<int, Room> m_rooms;
+
+public:
+    RoomManager() = default;
+    ~RoomManager() = default;
+
+    void createRoom(const LoggedUser &admin, const RoomData &data);
+    void deleteRoom(const int roomID);
+
+    RoomStatus getRoomStatus(const int roomID);
+    std::vector<RoomData> getRooms();
+    std::optional<Room &> getRoom(const int roomID);
+};
