6712f137e89547cafca1f6cd0b72738f6a489e04
Itay-He <hershkoitai@gmail.com>
2025-04-18 13:37:33 +0300

    Made StatisticsManager.h

--- Server/src/common/manager/StatisticsManager.h
+++ Server/src/common/manager/StatisticsManager.h
@@ -1,1 +1,19 @@
 # pragma once
+
+#include "infrastructure/db/IDatabase.h"
+#include <string>
+#include <vector>
+
+class StatisticsManager
+{
+private:
+	IDatabase *m_database;
+
+public:
+    StatisticsManager(const IDatabase *db);
+    ~StatisticsManager() = default;
+
+    std::vector<std::string &> &getHighScore();
+    std::vector<std::string &> &getUserStatistics(const std::string& username);
+
+};
