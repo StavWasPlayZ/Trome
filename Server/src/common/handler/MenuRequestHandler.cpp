6c24728a23907e16feebdd6fa10da68f6c3c55a6
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-21 15:38:28 +0300

    Fix unconventional namings of RoomData fields; Add questions count field to RoomData

--- Server/src/common/handler/MenuRequestHandler.cpp
+++ Server/src/common/handler/MenuRequestHandler.cpp
@@ -1,16 +1,69 @@
 #include "MenuRequestHandler.h"
 
+#include "RequestHandlerFactory.h"
+#include "codec/c2s/Request.h"
+
 MenuRequestHandler::MenuRequestHandler(const RequestHandlerFactory &handlerFactory) : IRequestHandler(handlerFactory)
 {}
 
 bool MenuRequestHandler::isRequestRelevant(const RequestInfo &info) const
 {
-    //TODO: Implement
-    return true;
+    switch (info.id)
+    {
+    case RequestCode::CREATE_ROOM:
+    case RequestCode::GET_ROOMS:
+    case RequestCode::GET_PLAYER_IN_ROOM:
+    case RequestCode::GET_HIGH_SCORES:
+    case RequestCode::GET_PERSONAL_STATISTICS:
+    case RequestCode::LOGOUT:
+        return true;
+
+    default: return false;
+    }
 }
 
 RequestResult MenuRequestHandler::handleRequest(const RequestInfo& info, const ProtocolRequest& request) const
 {
-    //TODO: Implement
-    return errorUnimplementedResult<MenuRequestHandler>();
+    RoomManager& rManager = m_handlerFactory.getRoomManager();
+    LoginManager& uManager = m_handlerFactory.getLoginManager();
+
+    switch (info.id)
+    {
+    case RequestCode::CREATE_ROOM: {
+        const CreateRoomRequest &req = static_cast<const CreateRoomRequest &>(request);
+
+        rManager.createRoom(
+            uManager.getUserByClient(info.client),
+            RoomData(
+                req.roomName,
+                RoomStatus::WAITING,
+                req.maxPlayers,
+                req.answerTimeout
+            )
+        );
+
+        // return RequestResult(
+        //     JsonResponsePacketSerializer::serializeResponse(
+        //         CreateRoomResponse(GeneralRoomStatus::SUCCESS, rManager.getRooms())
+        //     ),
+        //
+        //     new MenuRequestHandler(*this)
+        // );
+    }
+    case RequestCode::GET_ROOMS: {
+        return RequestResult(
+            JsonResponsePacketSerializer::serializeResponse(
+                GetRoomsResponse(GeneralRoomStatus::SUCCESS, rManager.getRooms())
+            ),
+
+            new MenuRequestHandler(*this)
+        );
+    }
+    case RequestCode::GET_PLAYER_IN_ROOM:
+    case RequestCode::GET_HIGH_SCORES:
+    case RequestCode::GET_PERSONAL_STATISTICS:
+    case RequestCode::LOGOUT:
+
+    default: throw std::invalid_argument("Unknown request ID");
+    }
 }
--- Server/src/common/infrastructure/RoomData.cpp
+++ Server/src/common/infrastructure/RoomData.cpp
@@ -5,12 +5,15 @@ unsigned int RoomData::globalId = 0;
 RoomData::RoomData(
     const std::string &name,
     const RoomStatus &status,
-    const unsigned int max_players, const unsigned int time_per_question
+    const int maxPlayers,
+    const int timePerQuestion,
+    const int questionsCount
 ) :
     name(name),
     id(globalId++),
     // REVIEW: Perhaps could be initialized directly.
     status(status),
-    maxPlayers(max_players),
-    timePerQuestion(time_per_question)
+    maxPlayers(maxPlayers),
+    timePerQuestion(timePerQuestion),
+    questionsCount(questionsCount)
 {}
