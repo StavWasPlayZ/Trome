3550b0d2d3a08afeddb7e695734f1ae69bf233e8
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-21 20:40:57 +0300

    Extract login conditional comment into logical constant for testV2.py

--- PythonTest/testV2.py
+++ PythonTest/testV2.py
@@ -1,13 +1,18 @@
 import communicator
 import socket
 
+USER_EXISTS = True
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(communicator.SERVER_INFO)
 
         try:
-            ## communicator.sendAndRecv(communicator.SIGNUP, communicator.JSON_TEST_SIGNUP, sock) ## do this if there isn't this user in db
-            communicator.sendAndRecv(communicator.LOGIN, communicator.JSON_TEST_LOGIN, sock) ## remove this if there isn't this user in db
+            if USER_EXISTS:
+                communicator.sendAndRecv(communicator.LOGIN, communicator.JSON_TEST_LOGIN, sock)
+            else:
+                communicator.sendAndRecv(communicator.SIGNUP, communicator.JSON_TEST_SIGNUP, sock)
+
             communicator.sendAndRecv(communicator.GET_PERSONAL_STATISTICS, communicator.JSON_GET_PERSONAL_STATS, sock)
         except:
             print("Something went wrong :(")
