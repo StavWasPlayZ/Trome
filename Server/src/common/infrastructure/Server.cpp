5ebfb79c22fc38917aef9ca6b7add1fb9b621050
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-21 23:14:04 +0300

    Initial questions are now fetched from opentdb.com; Closes #18

--- Server/src/common/infrastructure/Server.cpp
+++ Server/src/common/infrastructure/Server.cpp
@@ -1,6 +1,10 @@
 ﻿#include "Server.h"
 
+#include <httplib.h>
+#include <nlohmann/json.hpp>
+
 #include "db/SqliteDatabase.h"
+#include "infrastructure/Question.h"
 
 Server::Server() :
     m_database(SqliteDatabase::getInstance()),
@@ -9,6 +13,66 @@ Server::Server() :
     m_handlerFactory(this->m_loginManager, this->m_roomManager, this->m_statisticsManager, this->m_database),
     m_communicator(Communicator::getInstance(this->m_handlerFactory))
 {}
+
+void Server::populateQuestionsFromWeb(const int amount) const
+{
+    std::cout << "Populating DB with web questions...";
+
+    if (m_database.queryQuestionsCount() > 0)
+    {
+        std::cout << "DB already populated; Aborting operation" << std::endl;
+        return;
+    }
+
+    std::cout << "Fetching questions from web..." << std::endl;
+
+    httplib::Client client("https://opentdb.com");
+    const httplib::Result response = client.Get("/api.php?"
+        "category=18"
+        "&type=multiple"
+        "&amount=" + std::to_string(amount)
+    );
+
+    const nlohmann::json parsedResponse = nlohmann::json::parse(response->body);
+
+    const int resStatus = parsedResponse.at("response_code");
+
+    if (resStatus != 0)
+    {
+        std::cerr << "Fetching unsuccessful with status " << resStatus << std::endl;
+        return;
+    }
+
+    std::cout << "Fetching successful with status " << resStatus << std::endl;
+
+    m_database.addQuestions(deserializeWebQuestions(parsedResponse));
+
+    std::cout << "Successfully populated web questions" << std::endl;
+}
+
+std::vector<Question> Server::deserializeWebQuestions(const nlohmann::json &data)
+{
+    std::vector<Question> questions;
+
+    for (const nlohmann::json &questionRaw : data.at("results"))
+    {
+        std::vector<std::string> answers;
+        answers.reserve(4);
+
+        answers.push_back(questionRaw.at("correct_answer"));
+
+        for (const nlohmann::json &answerRaw : questionRaw.at("incorrect_answers"))
+        {
+            answers.push_back(answerRaw.get<std::string>());
+        }
+
+        const Question question(questionRaw.at("question"), answers);
+
+        questions.push_back(question);
+    }
+
+    return questions;
+}
 
 Server &Server::getInstance()
 {
@@ -19,6 +83,8 @@ Server &Server::getInstance()
 void Server::run() const
 {
 	m_database.open();
+    populateQuestionsFromWeb();
+
 	this->m_communicator.bindAndListen();
 }
 
--- Server/src/common/infrastructure/Server.h
+++ Server/src/common/infrastructure/Server.h
@@ -26,6 +26,13 @@ public:
 private:
     Server();
 
+    /**
+    * Populates the Questions table of the DB with
+    * questions from opentdb.com
+    */
+    void populateQuestionsFromWeb(int amount = 100) const;
+    static std::vector<Question> deserializeWebQuestions(const nlohmann::json& data);
+
 	IDatabase& m_database;
 
 	LoginManager m_loginManager;
