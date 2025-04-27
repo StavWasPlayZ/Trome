bedebb2712b4b1a8bfb400453bbc5084b0ba3fd5
Itay-He <hershkoitai@gmail.com>
2025-04-27 18:01:55 +0300

    Made RoomMemberRequestHandler.cpp

--- Server/src/common/handler/RoomMemberRequestHandler.h
+++ Server/src/common/handler/RoomMemberRequestHandler.h
@@ -7,14 +7,13 @@
 class RoomMemberRequestHandler : public IRequestHandler
 {
 public:
-    RoomMemberRequestHandler(const RequestHandlerFactory &handlerFactory, const Room &room, const RoomManager manager);
+    RoomMemberRequestHandler(const RequestHandlerFactory &handlerFactory, Room &room);
 
     bool isRequestRelevant(const RequestInfo &info) const;
     RequestResult handleRequest(const RequestInfo &info, const ProtocolRequest &request) const;
 private:
-    Room m_room;
-    RoomManager m_roomManager;
+    Room& m_room;
 
-    RequestResult leaveRoom(const RequestInfo &context, const LeaveRoomResponse &request) const;
-    RequestResult getRoomState(const RequestInfo &context, const GetRoomStateResponse &request) const;
+    RequestResult leaveRoom(const RequestInfo &info, const ProtocolRequest &request) const;
+    RequestResult getRoomState(const RequestInfo &info, const ProtocolRequest &request) const;
 };
