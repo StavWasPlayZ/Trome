531dd44fce7de5dc31c346082a6c7530549a9ccf
StavWasPlayZ <stavsolomon101@gmail.com>
2025-04-30 15:47:21 +0300

    Make TriviaButton's colors customizable; Customize play button colors

--- Client/Trivia/Controls/TriviaButton.axaml.cs
+++ Client/Trivia/Controls/TriviaButton.axaml.cs
@@ -1,13 +1,12 @@
 using Avalonia;
-using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 
 namespace Trivia.Controls;
 
 public class TriviaButton : TemplatedControl
 {
-    public static readonly StyledProperty<string> TextProperty =
-        AvaloniaProperty.Register<TriviaButton, string>(nameof(Text));
+    public static readonly StyledProperty<string> TextProperty = AvaloniaProperty.Register<TriviaButton, string>(
+        nameof(Text));
     
     public string Text
     {
