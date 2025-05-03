7f1b704f4a31d370d1b96ca9efaacde55d3032af
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-03 15:32:13 +0300

    Add Exit button behavior

--- Client/Trivia/ViewModels/Menu/MainMenuViewModel.cs
+++ Client/Trivia/ViewModels/Menu/MainMenuViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reactive;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using ReactiveUI;
 
 namespace Trivia.ViewModels.Menu;
@@ -6,6 +8,11 @@ namespace Trivia.ViewModels.Menu;
 public class MainMenuViewModel : PageViewModel
 {
     public ReactiveCommand<Unit, IRoutableViewModel> PlayCommand { get; }
+    public ReactiveCommand<Unit, Unit> ExitCommand { get; } = ReactiveCommand.Create(() =>
+    {
+        (Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)
+            ?.Shutdown();
+    });
     
     public MainMenuViewModel(IScreen hostScreen) : base(hostScreen)
     {
