5b3664f5310df93d49b9e0d2197f9c6b21d320fc
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-03 09:59:49 +0300

    Refactor MenuPanel's title as a property

--- Client/Trivia/Controls/MenuPanelExpanded.axaml.cs
+++ Client/Trivia/Controls/MenuPanelExpanded.axaml.cs
@@ -1,7 +1,16 @@
+using Avalonia;
 using Avalonia.Controls;
 
 namespace Trivia.Controls;
 
 public class MenuPanelExpanded : ContentControl
 {
+    public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<MenuPanelExpanded, string>(
+        nameof(Title));
+
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
 }
