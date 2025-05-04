63421d9a57198ea4b731e28ec9e50d117811b0d0
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-04 22:39:04 +0300

    Bind New Room button; Reformat style of New Room button; Fix main menu being TestView

--- Client/Trivia/ViewModels/Menu/JoinMenuViewModel.cs
+++ Client/Trivia/ViewModels/Menu/JoinMenuViewModel.cs
@@ -3,8 +3,20 @@ using ReactiveUI;
 
 namespace Trivia.ViewModels.Menu;
 
-public class JoinMenuViewModel(IScreen hostScreen) : PageViewModel(hostScreen)
+public class JoinMenuViewModel : PageViewModel
 {
     public ReactiveCommand<Unit, IRoutableViewModel>? BackButtonCommand { get; } =
         GetMainWindowViewModel()?.Router.NavigateBack;
+    
+    public ReactiveCommand<Unit, IRoutableViewModel> NewRoomButtonCommand { get; }
+    
+    public JoinMenuViewModel(IScreen hostScreen) : base(hostScreen)
+    {
+        NewRoomButtonCommand = ReactiveCommand.CreateFromObservable(() => NavigateTo(
+            //TODO: Have this:
+            // new CreateRoomViewModel(HostScreen))!
+            
+            new MainMenuViewModel(HostScreen))!
+        );
+    }
 }
