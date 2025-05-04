8d26345fc55f30a0cd47c336ae6e23683628f834
Itay-He <hershkoitai@gmail.com>
2025-05-04 19:30:53 +0300

    Made create room

---
+++ Client/Trivia/Views/Menu/CreateRoomView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Trivia.ViewModels.Menu;
+
+namespace Trivia.Views.Menu;
+
+public partial class CreateRoomView : PageViewControl<CreateRoomViewModel>
+{
+    public CreateRoomView()
+    {
+        InitializeComponent();
+    }
+}
