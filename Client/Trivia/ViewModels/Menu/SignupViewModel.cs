6c334eddc49eda5021a7728c3eda984201652876
Itay-He <hershkoitai@gmail.com>
2025-05-06 16:37:12 +0300

    Made signup + updated margins in login so it will be correct

---
+++ Client/Trivia/ViewModels/Menu/SignupViewModel.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Trivia.ViewModels.Menu;
+
+public class SignupViewModel(IScreen hostScreen) : PageViewModel(hostScreen)
+{
+    
+}
---
+++ Client/Trivia/Views/Menu/SignupView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Trivia.ViewModels.Menu;
+
+namespace Trivia.Views.Menu;
+
+public partial class SignupView : PageViewControl<SignupViewModel>
+{
+    public SignupView()
+    {
+        InitializeComponent();
+    }
+}
