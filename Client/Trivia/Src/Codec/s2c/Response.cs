8e36c6e9f6055f312804d1fdac359c633692c7dc
Itay-He <hershkoitai@gmail.com>
2025-05-08 22:25:55 +0300

    Added enums in Response.cs

---
+++ Client/Trivia/Src/Codec/s2c/Response.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace Trivia.Codec.s2c
+{
+    namespace Response
+    {
+        public enum ResponseCode : byte
+        {
+            Error = 0,
+            Login,
+            Signup,
+            Logout,
+            JoinRoom,
+            CreateRoom,
+            GetRooms,
+            GetPlayerInRoom,
+            GetHighScores,
+            GetPersonalStatistics
+        }
+
+        public enum GenericResponseStatus : uint
+        {
+            Success = 1,
+            Error = 0
+        }
+
+        public enum ConsumingResponseStatus  : uint
+        {
+            Success = 1,
+            ErrorUnknownResource,
+            ErrorInternal = 0
+        }
+    }
+}
