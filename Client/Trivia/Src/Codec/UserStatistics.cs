f23ff67bdf0f97b70936cbe401ad25ae3f54f84c
Itay-He <hershkoitai@gmail.com>
2025-05-08 23:05:46 +0300

    Made UserStatistics.cs

---
+++ Client/Trivia/Src/Codec/UserStatistics.cs
@@ -0,0 +1,22 @@
+﻿namespace Trivia.Codec;
+
+public class UserStatistics
+{
+    public UserStatistics(int points, int gamesPlayed, int questionsAnswered, int questionsAnsweredCorrectly,
+        int timeOnQuestionOverall, int timeOnQuestionsAvg)
+    {
+        this.Points = points;
+        this.GamesPlayed = gamesPlayed;
+        this.QuestionsAnswered = questionsAnswered;
+        this.QuestionsAnsweredCorrectly = questionsAnsweredCorrectly;
+        this.TimeOnQuestionOverall = timeOnQuestionOverall;
+        this.TimeOnQuestionAvg = timeOnQuestionsAvg;
+    }
+    
+    public readonly int Points;
+    public readonly int GamesPlayed;
+    public readonly int QuestionsAnswered;
+    public readonly int QuestionsAnsweredCorrectly;
+    public readonly int TimeOnQuestionOverall;
+    public readonly int TimeOnQuestionAvg;
+}
