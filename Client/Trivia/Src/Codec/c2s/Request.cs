afaa64b63fc228ae0324603b65f41ecf0976bd65
Itay-He <hershkoitai@gmail.com>
2025-05-08 21:38:14 +0300

    Added Request.cs and the RequestCode

---
+++ Client/Trivia/Src/Codec/c2s/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace Trivia.Codec.c2s
+{
+    namespace Request
+    {
+        public enum RequestCode : byte
+        {
+            Logout = 0,
+            Login = 1,
+            Signup,
+            GetPlayersInRoom,
+            JoinRoom,
+            CreateRoom,
+            GetRooms,
+            GetHighScores,
+            GetPersonalStatistics
+        }
+    }
+}
