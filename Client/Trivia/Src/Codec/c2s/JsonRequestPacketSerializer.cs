0a6b9efae2a09d3bc45bb5295ab56f1e14365538
Itay-He <hershkoitai@gmail.com>
2025-05-09 18:03:08 +0300

    Made serializeJsonToProtocol

--- Client/Trivia/Src/Codec/c2s/JsonRequestPacketSerializer.cs
+++ Client/Trivia/Src/Codec/c2s/JsonRequestPacketSerializer.cs
@@ -10,5 +10,23 @@ using Newtonsoft.Json;
 
 public class JsonRequestPacketSerializer
 {
-    
+    private const uint SizeCode = 1;
+    private const uint SizeJsonLen = 4;
+    public static string serializeJsonToProtocol(ResponseCode code, JsonObject data)
+    {
+        string dataStr = JsonConvert.SerializeObject(data);
+        byte codeByte = (byte)code;
+        byte[] strBytes = Encoding.UTF8.GetBytes(dataStr);
+        byte[] lenBytes = BitConverter.GetBytes(strBytes.Length);
+
+        if (!BitConverter.IsLittleEndian)
+            Array.Reverse(lenBytes); // Makes sure that little-endian if needed
+
+        byte[] result = new byte[1 + 4 + strBytes.Length];
+        result[0] = codeByte;
+        Array.Copy(lenBytes, 0, result, SizeCode, SizeJsonLen);
+        Array.Copy(strBytes, 0, result, SizeCode + SizeJsonLen, strBytes.Length);
+
+        return Encoding.Latin1.GetString(result);
+    }
 }
