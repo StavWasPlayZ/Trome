82a2b630266612456820b632ad1da797e6d3a954
Itay-He <hershkoitai@gmail.com>
2025-05-09 17:38:05 +0300

    Added JsonRequestPacketDeserializer.cs + made ReadJson

---
+++ Client/Trivia/Src/Codec/c2s/JsonRequestPacketDeserializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Trivia.Codec.c2s;
+using System;
+using System.Text;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+public class JsonRequestPacketDeserializer
+{
+    public static JsonObject ReadJson(byte[] data, int jsonLen)
+    {
+	    if (jsonLen <= 0 || data == null || data.Length < jsonLen)
+	    {
+		    throw new ArgumentException("Invalid JSON length or data buffer.");
+	    }
+	    
+	    string jsonString = Encoding.UTF8.GetString(data, 0, jsonLen);
+	    JsonNode? node = JsonNode.Parse(jsonString);
+	    
+	    if (node is JsonObject obj)
+	    {
+		    return obj;
+	    }
+
+	    throw new InvalidOperationException("Parsed JSON is not an object.");
+    }
+}
