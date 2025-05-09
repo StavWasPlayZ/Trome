e4646dd6488f0ed95cf29493d0653d289b6ea4a9
Itay-He <hershkoitai@gmail.com>
2025-05-09 18:19:29 +0300

    Made Deserialize

---
+++ Client/Trivia/Src/Codec/s2c/JsonRequestPacketDeserializer.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Trivia.Codec.s2c.Response;
+
+namespace Trivia.Codec.s2c;
+
+public class JsonRequestPacketDeserializer
+{
+    /// <summary>
+    /// T - Protocol Response
+    /// </summary>
+    public static T Deserialize<T>(string data)
+    {
+        return JsonConvert.DeserializeObject<T>(data);
+    }
+}
