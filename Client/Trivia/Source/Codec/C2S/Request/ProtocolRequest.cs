6e2d149456a629fa2aaee94751de1d1204c9c6e0
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-11 21:59:21 +0300

    Make JsonRequestPacketSerializer part of ProtocolRequest

--- Client/Trivia/Source/Codec/C2S/Request/ProtocolRequest.cs
+++ Client/Trivia/Source/Codec/C2S/Request/ProtocolRequest.cs
@@ -1,5 +1,36 @@
-﻿namespace Trivia.Codec.C2S.Request;
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Trivia.Codec.C2S.Request;
 
 public abstract record ProtocolRequest(
     RequestCode Code
-);
+)
+{
+    private const uint SizeCode = 1;
+    private const uint SizeJsonLen = 4;
+    
+    public byte[] SerializeRequest()
+    {
+        return SerializeJsonToProtocol(JsonConvert.SerializeObject(this));
+    }
+    
+    private byte[] SerializeJsonToProtocol(string data)
+    {
+        var strBytes = Encoding.UTF8.GetBytes(data);
+        var lenBytes = BitConverter.GetBytes(strBytes.Length);
+
+        if (!BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(lenBytes); // Makes sure that little-endian, if needed
+        }
+
+        var result = new byte[1 + 4 + strBytes.Length];
+        result[0] = (byte) Code;
+        Array.Copy(lenBytes, 0, result, SizeCode, SizeJsonLen);
+        Array.Copy(strBytes, 0, result, SizeCode + SizeJsonLen, strBytes.Length);
+
+        return result;
+    }
+}
