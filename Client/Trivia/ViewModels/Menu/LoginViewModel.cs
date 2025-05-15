97da7027d52d9ad6b90f01285f31b5fd5f588175
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-15 21:13:20 +0300

    Migrate Signup validation system to Login view

--- Client/Trivia/ViewModels/Menu/LoginViewModel.cs
+++ Client/Trivia/ViewModels/Menu/LoginViewModel.cs
@@ -1,17 +1,53 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Reactive;
 using ReactiveUI;
 
 namespace Trivia.ViewModels.Menu;
 
-public class LoginViewModel(IScreen hostScreen) : AuthViewModel(hostScreen)
+public class LoginViewModel : AuthViewModel
 {
-    public ReactiveCommand<Unit, IRoutableViewModel> ToSignupCommand { get; } = NavigateAndResetReactiveCommand(
-        () => new SignupViewModel(hostScreen)
-    );
+    public ReactiveCommand<Unit, IRoutableViewModel> ToSignupCommand { get; }
+    
+    public LoginViewModel(IScreen hostScreen) : base(hostScreen)
+    {
+        ToSignupCommand = NavigateAndResetReactiveCommand(
+            () => new SignupViewModel(hostScreen)
+        );
+        
+        this
+            .WhenAnyValue(
+                vm => vm.Username,
+                vm => vm.Password
+            )
+            .Subscribe(_ => UpdateMayAuthenticate());
+    }
+    
 
     protected override void UpdateMayAuthenticate()
     {
-        throw new NotImplementedException();
+        MayAuthenticate =
+            !string.IsNullOrEmpty(Username)
+            && !string.IsNullOrEmpty(Password);
     }
+    
+    
+    private string? _username;
+
+    [Required]
+    public string? Username
+    {
+        get => _username;
+        set => this.RaiseAndSetIfChanged(ref _username, value);
+    }
+    
+    private string? _password;
+
+    [Required]
+    public string? Password
+    {
+        get => _password;
+        set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+    
 }
