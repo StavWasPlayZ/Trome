3ef6f53f97071b46bfb8b85c98cfec67f45fb14e
Itay-He <hershkoitai@gmail.com>
2025-05-18 19:32:35 +0300

    Made RefreshThread

--- Client/Trivia/ViewModels/Menu/JoinMenuViewModel.cs
+++ Client/Trivia/ViewModels/Menu/JoinMenuViewModel.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reactive;
 using System.Reactive.Disposables;
+using System.Threading;
+using System.Threading.Tasks;
 using ReactiveUI;
+using Trivia.Codec.C2S.Request;
+using Trivia.Codec.S2C.Response;
 using Trivia.Models;
 using Trivia.ViewModels.Game;
 
@@ -12,6 +17,8 @@ public class JoinMenuViewModel : PageViewModel, IActivatableViewModel
     public ViewModelActivator Activator { get; } = new();
     
     private List<Room> rooms = [];
+    private bool _isDisposed;
+    private readonly int refreshTime = 3000;
 
     public JoinMenuViewModel(IScreen hostScreen) : base(hostScreen)
     {
@@ -22,12 +29,27 @@ public class JoinMenuViewModel : PageViewModel, IActivatableViewModel
             () => new JoinedRoomViewModel(hostScreen)
         );
 
-        this.WhenActivated((CompositeDisposable _) =>
+        this.WhenActivated((CompositeDisposable disposables) =>
         {
             //TODO: Create thread to refresh rooms
+            Disposable
+                .Create(() => _isDisposed = true)
+                .DisposeWith(disposables);
+
+            _ = RefreshThread();
         });
     }
 
+    private async Task RefreshThread()
+    {
+        while (!_isDisposed)
+        {
+            var response = await Communicator.Instance.SendRequestAwaitResponse<GetRoomsResponse>(new GetRoomsRequest());
+
+            rooms = response.Rooms;
+            Thread.Sleep(refreshTime);
+        }
+    }
 
     public ReactiveCommand<Unit, IRoutableViewModel> NewRoomButtonCommand { get; }
     
