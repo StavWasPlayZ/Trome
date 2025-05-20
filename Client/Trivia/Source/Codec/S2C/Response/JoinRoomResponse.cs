d7fb8da51d6d481d4de54e60455f4402aed34b9a
Itay-He <hershkoitai@gmail.com>
2025-05-20 18:21:01 +0300

    Updated client-side to have the data

--- Client/Trivia/Source/Codec/S2C/Response/JoinRoomResponse.cs
+++ Client/Trivia/Source/Codec/S2C/Response/JoinRoomResponse.cs
@@ -3,5 +3,10 @@ using Trivia.Codec.S2C.Response.Status;
 namespace Trivia.Codec.S2C.Response;
 
 public record JoinRoomResponse(
-    ConsumingResponseStatus Status
+    ConsumingResponseStatus Status,
+    string Name,
+    int MaxPlayers,
+    int QuestionCount,
+    int SecsPerQuestion,
+    RoomStatus RoomStatus
 ) : ProtocolResponse<ConsumingResponseStatus>(Status);
