8ca782138341e54f16b789c7cd26886d20f97aa1
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-23 14:01:24 +0300

    Update RoomUserListEntry to include styles for Admin, Current User, Both and Empty

--- Client/Trivia/Controls/ListEntries/RoomUserListEntry.axaml.cs
+++ Client/Trivia/Controls/ListEntries/RoomUserListEntry.axaml.cs
@@ -1,17 +1,44 @@
 using Avalonia;
 using Avalonia.Controls.Primitives;
-using Trivia.Models.User;
+using Trivia.Models.Raw;
 
 namespace Trivia.Controls.ListEntries;
 
 public class RoomUserListEntry : TemplatedControl
 {
-    public static readonly StyledProperty<RoomUserModel> UserProperty = AvaloniaProperty.Register<RoomUserListEntry, RoomUserModel>(
-        nameof(RoomUserModel));
+    public static readonly StyledProperty<User?> UserProperty = AvaloniaProperty.Register<RoomUserListEntry, User?>(
+        nameof(User));
 
-    public RoomUserModel User
+    public User? User
     {
         get => GetValue(UserProperty);
         set => SetValue(UserProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsAdminProperty = AvaloniaProperty.Register<RoomUserListEntry, bool>(
+        nameof(IsAdmin));
+
+    public bool IsAdmin
+    {
+        get => GetValue(IsAdminProperty);
+        set => SetValue(IsAdminProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsCurrentUserProperty = AvaloniaProperty.Register<RoomUserListEntry, bool>(
+        nameof(IsCurrentUser));
+
+    public bool IsCurrentUser
+    {
+        get => GetValue(IsCurrentUserProperty);
+        set => SetValue(IsCurrentUserProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsEmptySlotProperty = AvaloniaProperty.Register<RoomUserListEntry, bool>(
+        nameof(IsEmptySlot));
+
+    public bool IsEmptySlot
+    {
+        get => GetValue(IsEmptySlotProperty);
+        set => SetValue(IsEmptySlotProperty, value);
     }
 }
