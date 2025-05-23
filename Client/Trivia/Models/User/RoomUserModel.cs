5f80a383e88dbb318ccb016050b23c3ff1d1e069
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-23 14:39:14 +0300

    Make CreateRoomViewModel use an actual Users list

--- Client/Trivia/Models/User/RoomUserModel.cs
+++ Client/Trivia/Models/User/RoomUserModel.cs
@@ -3,4 +3,21 @@ namespace Trivia.Models.User;
 public class RoomUserModel : UserModel
 {
     public bool IsAdmin { get; init; }
+
+
+    public static RoomUserModel FromUserModel(UserModel userModel, bool isAdmin)
+    {
+        return new RoomUserModel
+        {
+            Id = userModel.Id,
+            Username = userModel.Username,
+            IsCurrentUser = userModel.IsCurrentUser,
+            IsAdmin = isAdmin
+        };
+    }
+    
+    public static RoomUserModel FromUser(Raw.User user, bool isCurrentUser, bool isAdmin)
+    {
+        return FromUserModel(FromUser(user, isCurrentUser), isAdmin);
+    }
 }
--- Client/Trivia/Models/User/UserModel.cs
+++ Client/Trivia/Models/User/UserModel.cs
@@ -3,4 +3,15 @@ namespace Trivia.Models.User;
 public class UserModel : Raw.User
 {
     public bool IsCurrentUser { get; init; }
+    
+    
+    public static UserModel FromUser(Raw.User user, bool isCurrentUser)
+    {
+        return new UserModel
+        {
+            Id = user.Id,
+            Username = user.Username,
+            IsCurrentUser = isCurrentUser
+        };
+    }
 }
