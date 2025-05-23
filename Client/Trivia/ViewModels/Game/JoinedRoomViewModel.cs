d470854ceecc792f00bceeb53c9fc21527125daf
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-23 23:25:09 +0300

    Revert "Acknowledge clients' disconnections"
    This reverts commit 12052cf833e6c72c6b40154d8e5c59b4e9126ba5.

--- Client/Trivia/ViewModels/Game/JoinedRoomViewModel.cs
+++ Client/Trivia/ViewModels/Game/JoinedRoomViewModel.cs
@@ -1,28 +1,16 @@
 ﻿using System.Collections.Generic;
-using System.Reactive;
 using ReactiveUI;
-using Trivia.Codec.C2S.Request.Packets;
-using Trivia.Codec.S2C.Response.Packets;
 using Trivia.Models.Raw;
 
 namespace Trivia.ViewModels.Game;
 
 public class JoinedRoomViewModel : RoomViewModel
 {
-    public ReactiveCommand<Unit, Unit> LeaveRoomCommand { get; }
-    
     public JoinedRoomViewModel(IScreen hostScreen, Room room, List<User> players) : base(hostScreen, room, players)
     {
-        LeaveRoomCommand = ReactiveCommand.CreateFromTask(async () =>
-        {
-            await Comm.SendRequestAwaitResponse<LeaveRoomResponse>(new LeaveRoomRequest());
-            
-            NavigateBackCommand!.Execute();
-        });
     }
 
     public JoinedRoomViewModel()
     {
-        LeaveRoomCommand = NoOpCommand;
     }
 }
