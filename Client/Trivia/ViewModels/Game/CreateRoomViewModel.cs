b16e97858b72cc6057a263ca6521b73d382b90a4
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-24 23:17:11 +0300

    Link CreateRoomView fields to ViewModel

--- Client/Trivia/ViewModels/Game/CreateRoomViewModel.cs
+++ Client/Trivia/ViewModels/Game/CreateRoomViewModel.cs
@@ -12,6 +12,11 @@ public class CreateRoomViewModel : RoomViewModel
 
     public CreateRoomViewModel(IScreen hostScreen, Room room) : base(hostScreen, room)
     {
+        _name = room.Data.Name;
+        _questions = room.Data.QuestionsCount;
+        _secsPerQuestion = room.Data.TimePerQuestionSecs;
+        _maxPlayers = room.Data.MaxPlayers;
+        
         CloseRoomCommand = ReactiveCommand.CreateFromTask(async () =>
         {
             await Comm.SendRequestAwaitResponse<CloseRoomResponse>(new CloseRoomRequest());
@@ -22,6 +27,48 @@ public class CreateRoomViewModel : RoomViewModel
 
     public CreateRoomViewModel()
     {
+        _name = "ROOM NAME";
+        _questions = 20;
+        _secsPerQuestion = 10;
+        _maxPlayers = 10;
+        
         CloseRoomCommand = NoOpCommand;
+    }
+
+
+
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => this.RaiseAndSetIfChanged(ref _name, value);
+    }
+    
+    
+    private int _questions;
+
+    public int Questions
+    {
+        get => _questions;
+        set => this.RaiseAndSetIfChanged(ref _questions, value);
+    }
+    
+    
+    private int _secsPerQuestion;
+
+    public int SecsPerQuestion
+    {
+        get => _secsPerQuestion;
+        set => this.RaiseAndSetIfChanged(ref _secsPerQuestion, value);
+    }
+
+
+    private int _maxPlayers;
+
+    public int MaxPlayers
+    {
+        get => _maxPlayers;
+        set => this.RaiseAndSetIfChanged(ref _maxPlayers, value);
     }
 }
