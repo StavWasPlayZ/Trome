a24eb2eead2aa3c9cabc3e78cd9f4cef1b39d662
Itay-He <hershkoitai@gmail.com>
2025-05-25 19:24:53 +0300

    Made new responses

---
+++ Client/Trivia/Source/Codec/S2C/Response/Packets/GetGameResultResponse.cs
@@ -0,0 +1,7 @@
+﻿using Trivia.Codec.S2C.Objects;
+
+namespace Trivia.Codec.S2C.Response.Packets;
+
+public record GetGameResultResponse(
+    PlayerResult[] Results
+) : ProtocolResponse;
---
+++ Client/Trivia/Source/Codec/S2C/Response/Packets/GetQuestionResponse.cs
@@ -0,0 +1,9 @@
+﻿using Trivia.Codec.S2C.Objects;
+
+namespace Trivia.Codec.S2C.Response.Packets;
+
+public record GetQuestionResponse(
+    string Question,
+    string[] Answers,
+    int Rotation
+) : ProtocolResponse;
