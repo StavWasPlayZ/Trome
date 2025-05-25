a6627024cc0f57df08c5381cefde6dc73eda7213
Itay-He <hershkoitai@gmail.com>
2025-05-25 19:35:07 +0300

    Init the FinishedEarlyView related files

---
+++ Client/Trivia/ViewModels/Game/FinishedEarlyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Reactive;
+using ReactiveUI;
+using Trivia.Codec.C2S.Request.Packets;
+using Trivia.Codec.S2C;
+using Trivia.Codec.S2C.Notification.Packets;
+using Trivia.Codec.S2C.Response.Packets;
+using Trivia.Models.Raw;
+
+namespace Trivia.ViewModels.Game;
+
+public class FinishedEarlyViewModel : PageViewModel
+{
+    public FinishedEarlyViewModel(IScreen hostScreen) : base(hostScreen)
+    {}
+    
+    public FinishedEarlyViewModel() : base()
+    {}
+}
---
+++ Client/Trivia/Views/Game/FinishedEarlyView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Trivia.Views.Game;
+
+public partial class FinishedEarlyView : Window
+{
+    public FinishedEarlyView()
+    {
+        InitializeComponent();
+    }
+}
