45629e712fdefe4a29079ddf81e27670447d3199
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-28 20:24:27 +0300

    Randomize early splash screen message

--- Client/Trivia/ViewModels/Game/FinishedEarlyViewModel.cs
+++ Client/Trivia/ViewModels/Game/FinishedEarlyViewModel.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Reactive;
-using ReactiveUI;
-using Trivia.Codec.C2S.Request.Packets;
-using Trivia.Codec.S2C;
-using Trivia.Codec.S2C.Notification.Packets;
-using Trivia.Codec.S2C.Response.Packets;
-using Trivia.Models.Raw;
+﻿using ReactiveUI;
 
 namespace Trivia.ViewModels.Game;
 
 public class FinishedEarlyViewModel : PageViewModel
 {
+    private static readonly string[] EndingLines = [
+        "You await the inferiors before you...",
+        "You thought they gave you all they had, yet they knew naught...",
+        "\"Is that all?,\" you think to yourself...",
+        "“I held back,” you think to yourself...",
+        "You suppressed your power, lest the world trembled once more...",
+        "They call it a game, but to you, it's merely breathing...",
+        "They trained for years, but they never trained for you...",
+        "While they reached for greatness, you had already forgotten it...",
+        "Their struggle was just your warm-up...",
+        "You thought you faced their final form. How quaint...",
+    ];
+
+    public string EndingLine { get; } = EndingLines[App.Random.Next(EndingLines.Length)];
+
+
     public FinishedEarlyViewModel(IScreen hostScreen) : base(hostScreen)
     {}
     
