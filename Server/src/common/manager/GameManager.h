803351defa3e7cf455fa715e44fb349c1da58d5a
Itay-He <hershkoitai@gmail.com>
2025-05-29 16:21:43 +0300

    Made GameManager.h

--- Server/src/common/manager/GameManager.h
+++ Server/src/common/manager/GameManager.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <vector>
+
+#include "infrastructure/db/IDatabase.h"
+#include "infrastructure/Game.h"
+#include "infrastructure/Room.h"
+
+class GameManager
+{
+public:
+    explicit GameManager(const IDatabase &m_database);
+
+	Game &createGame(Room &room);
+
+	Game getGameByRoom(const Room &room);
+
+	void deleteGame(Room &room);
+
+private:
+	const IDatabase *m_database;
+	std::vector<Game &> m_games;
+};
