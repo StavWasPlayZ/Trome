b32122761bc477e9123969e9d54188c3560327cb
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-29 14:46:16 +0300

    Add 2nd place onward displays

--- Client/Trivia/Views/Game/AfterGameView.axaml.cs
+++ Client/Trivia/Views/Game/AfterGameView.axaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Avalonia.Controls;
-using Avalonia.VisualTree;
-using Trivia.ViewModels.Game;
+﻿using Trivia.ViewModels.Game;
 
 namespace Trivia.Views.Game;
 
@@ -10,10 +7,5 @@ public partial class AfterGameView : PageViewControl<AfterGameViewModel>
     public AfterGameView()
     {
         InitializeComponent();
-    }
-
-    private void OnLayoutUpdated(object? sender, EventArgs e)
-    {
-        HalfHeight.Height = (this.GetVisualRoot() as Window)!.Bounds.Height / 2;
     }
 }
