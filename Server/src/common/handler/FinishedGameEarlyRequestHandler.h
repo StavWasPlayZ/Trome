44951a1d05219a083f9785b340e3afa3300282f1
Itay-He <hershkoitai@gmail.com>
2025-05-30 19:52:16 +0300

    Made the handler header

--- Server/src/common/handler/FinishedGameEarlyRequestHandler.h
+++ Server/src/common/handler/FinishedGameEarlyRequestHandler.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "IRequestHandler.h"
+#include "codec/c2s/request/Request.h"
+
+class RequestHandlerFactory; // Circle dependency
+
+class FinishedGameEarlyRequestHandler : public IRequestHandler
+{
+public:
+    explicit FinishedGameEarlyRequestHandler(const RequestHandlerFactory &handlerFactory);
+
+    bool isRequestRelevant(const RequestInfo &info) const override;
+
+    RequestResult handleRequest(const RequestInfo &info, const ProtocolRequest &request) const override;
+
+private:
+    RequestResult leaveGame(const RequestInfo &info, const LeaveGameRequest &request) const;
+};
