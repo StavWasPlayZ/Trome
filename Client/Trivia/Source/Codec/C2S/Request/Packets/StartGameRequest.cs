7178687fc2748149f1ca5ff3d9064c8345c7c508
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-30 23:20:26 +0300

    Revert "Migrate server codec changes to client"
    This reverts commit 52e79ec7424392ae8d828f94d3b62eb5f2df25fd.

--- Client/Trivia/Source/Codec/C2S/Request/Packets/StartGameRequest.cs
+++ Client/Trivia/Source/Codec/C2S/Request/Packets/StartGameRequest.cs
@@ -1,10 +1,6 @@
-﻿using Trivia.Models.Raw;
+﻿namespace Trivia.Codec.C2S.Request.Packets;
 
-namespace Trivia.Codec.C2S.Request.Packets;
-
-public readonly record struct StartGameRequest(
-    RoomData Data
-) : IProtocolRequest
+public readonly record struct StartGameRequest() : IProtocolRequest
 {
     public RequestCode Code { get; init; } = RequestCode.StartGame;
 }
--- Client/Trivia/Source/Codec/S2C/Notification/Packets/GameStartedNotification.cs
+++ Client/Trivia/Source/Codec/S2C/Notification/Packets/GameStartedNotification.cs
@@ -1,7 +1,3 @@
-using Trivia.Models.Raw;
-
 namespace Trivia.Codec.S2C.Notification.Packets;
 
-public readonly record struct GameStartedNotification(
-    RoomData Data
-) : IProtocolNotification;
+public readonly record struct GameStartedNotification : IProtocolNotification;
