6ef6f654cb187b6516f9c35ab2eac61805e9fe23
StavWasPlayZ <stavsolomon101@gmail.com>
2025-05-31 10:51:56 +0300

    Hook relevant GameView elements to ViewModel

--- Client/Trivia/Models/Raw/Question.cs
+++ Client/Trivia/Models/Raw/Question.cs
@@ -6,4 +6,16 @@ public class Question
 {
     public required string Prompt { get; init; }
     public required List<string> Answers { get; init; }
+
+    
+    public static Question MockQuestion = new Question
+    {
+        Prompt = "Lorem ipsum etc i dont remember it all but here is some multilined text",
+        Answers = [
+            "Answer 1",
+            "Answer 2",
+            "Answer 3",
+            "Answer 4"
+        ]
+    };
 }
