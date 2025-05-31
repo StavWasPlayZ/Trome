93715a18318ea31a3d37d80f5348085e197a6393
Itay-He <hershkoitai@gmail.com>
2025-05-31 13:54:43 +0300

    Updated client side's GameEndedNotification

--- Client/Trivia/Source/Codec/S2C/Notification/Packets/GameEndedNotification.cs
+++ Client/Trivia/Source/Codec/S2C/Notification/Packets/GameEndedNotification.cs
@@ -1,3 +1,5 @@
+using Trivia.Codec.S2C.Objects;
+
 namespace Trivia.Codec.S2C.Notification.Packets;
 
-public readonly record struct GameEndedNotification : IProtocolNotification;
+public readonly record struct GameEndedNotification(PlayerResult[] Results) : IProtocolNotification;
