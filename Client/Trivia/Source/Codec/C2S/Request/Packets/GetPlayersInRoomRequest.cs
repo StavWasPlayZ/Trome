31f213e1a100cfb0d76c1d89aac746b9c82a5395
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-01 21:16:45 +0300

    Fix not providing any room ID to Join command

--- Client/Trivia/Source/Codec/C2S/Request/Packets/GetPlayersInRoomRequest.cs
+++ Client/Trivia/Source/Codec/C2S/Request/Packets/GetPlayersInRoomRequest.cs
@@ -1,8 +1,6 @@
 namespace Trivia.Codec.C2S.Request.Packets;
 
-public readonly record struct GetPlayersInRoomRequest(
-    int RoomId
-) : IProtocolRequest
+public readonly record struct GetPlayersInRoomRequest() : IProtocolRequest
 {
     public RequestCode Code { get; init; } = RequestCode.GetPlayersInRoom;
 }
