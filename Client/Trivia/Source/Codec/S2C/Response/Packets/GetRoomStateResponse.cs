8b1b6ac89e56ba3bc007fcd9a48809e97c76c4a8
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-01 18:01:06 +0300

    Fix packet fields naming mismatch between server & client

--- Client/Trivia/Source/Codec/S2C/Response/Packets/GetRoomStateResponse.cs
+++ Client/Trivia/Source/Codec/S2C/Response/Packets/GetRoomStateResponse.cs
@@ -3,5 +3,5 @@
 namespace Trivia.Codec.S2C.Response.Packets;
 
 public readonly record struct GetRoomStateResponse(
-    RoomModel RoomModel
+    RoomModel Room
 ) : IProtocolResponse;
--- Client/Trivia/Source/Codec/S2C/Response/Packets/JoinRoomResponse.cs
+++ Client/Trivia/Source/Codec/S2C/Response/Packets/JoinRoomResponse.cs
@@ -4,6 +4,6 @@ using Trivia.Models.Raw;
 namespace Trivia.Codec.S2C.Response.Packets;
 
 public readonly record struct JoinRoomResponse(
-    RoomModel RoomModel,
+    RoomModel Room,
     ImmutableList<User> Players
 ) : IProtocolResponse;
