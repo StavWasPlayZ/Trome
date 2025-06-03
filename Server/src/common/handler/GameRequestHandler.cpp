bba3520840a992d9f3d4cb6e97798f67a4d5949f
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-03 20:20:16 +0300

    Fix not handling edge case where last player to finish leaves

--- Server/src/common/handler/GameRequestHandler.cpp
+++ Server/src/common/handler/GameRequestHandler.cpp
@@ -116,6 +116,11 @@ RequestResult GameRequestHandler::leaveGame(const RequestInfo &info, const Leave
     else
     {
         room.removeUser(getUserByInfo(info));
+
+        if (this->m_game.isGameComplete())
+        {
+            handleLastPlayerFinished(info);
+        }
     }
 
     return RequestResult(
