4daa7d24ba259d2e0b41bd02ee42878fb8d60b07
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-05 12:24:31 +0300

    Migrate server changes

--- Client/Trivia/Source/Codec/S2C/Response/Packets/Impl/IQuestionResponse.cs
+++ Client/Trivia/Source/Codec/S2C/Response/Packets/Impl/IQuestionResponse.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+using Trivia.Codec.S2C.Objects;
 using Trivia.Models;
 
 namespace Trivia.Codec.S2C.Response.Packets.Impl;
@@ -6,4 +8,5 @@ public interface IQuestionResponse : IProtocolResponse
 {
     QuestionModel? Question { get; init; }
     int Points { get; init; }
+    ImmutableList<PlayerResult>? Results { get; init; }
 }
