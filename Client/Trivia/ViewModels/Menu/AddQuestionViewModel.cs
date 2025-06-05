4de7af7b9c9a28780b0cb892e79954c50ded954c
Itay-He <hershkoitai@gmail.com>
2025-06-05 17:43:08 +0300

    Connected view to view model

--- Client/Trivia/ViewModels/Menu/AddQuestionViewModel.cs
+++ Client/Trivia/ViewModels/Menu/AddQuestionViewModel.cs
@@ -1,6 +1,36 @@
-﻿namespace Trivia.ViewModels.Menu;
+﻿using System.Collections.Generic;
+using System.Reactive;
+using ReactiveUI;
+
+namespace Trivia.ViewModels.Menu;
 
 public class AddQuestionViewModel : PageViewModel
 {
+    public ReactiveCommand<Unit, Unit> AddQuestionCommand { get; }
+    private string? _prompt;
+    public string? Prompt
+    {
+        get => _prompt;
+        set => this.RaiseAndSetIfChanged(ref _prompt, value);
+    }
     
+    private List<string> _answers = [];
+    public List<string> Answers
+    {
+        get => _answers;
+        set => this.RaiseAndSetIfChanged(ref _answers, value);
+    }
+    
+    public AddQuestionViewModel(IScreen hostScreen) : base(hostScreen)
+    {
+        AddQuestionCommand = ReactiveCommand.CreateFromTask(async _ =>
+        {
+            // TODO
+        });
+    }
+
+    public AddQuestionViewModel()
+    {
+        AddQuestionCommand = NoOpCommand;
+    }
 }
