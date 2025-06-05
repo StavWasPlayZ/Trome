8d02eeec89826ccfbdb928a95d3706a60b28748e
Itay-He <hershkoitai@gmail.com>
2025-06-05 19:46:05 +0300

    Made the prev fix better by not changing the avg calc

--- Server/src/common/infrastructure/GameData.cpp
+++ Server/src/common/infrastructure/GameData.cpp
@@ -64,7 +64,8 @@ std::chrono::milliseconds GameData::getTimeSinceQuestionRoll() const
 
 std::chrono::seconds GameData::getAverageAnswerTime() const
 {
-    return this->averageAnswerTime;
+    // if the time is 0 (rounded) return 1 sec
+    return this->averageAnswerTime.count() > 0 ? this->averageAnswerTime : std::chrono::seconds(1);
 }
 
 std::chrono::seconds GameData::getPlaytime() const
@@ -109,11 +110,6 @@ void GameData::submitRoundTime()
         (this->averageAnswerTime * currentQuestionIndex + roundTime)
         / (currentQuestionIndex + 1)
     );
-
-    if (this->averageAnswerTime.count() == 0)
-    {
-        this->averageAnswerTime = std::chrono::seconds(1);
-    }
 }
 
 UserQuestion::UserQuestion(const Question &question, const int rotation) :
