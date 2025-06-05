8360df5b13dac63c19fd9847a0be4809657753ba
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-05 13:33:55 +0300

    Make AfterGameViewModel accept PlayerResults

--- Client/Trivia/ViewModels/Game/AfterGameViewModel.cs
+++ Client/Trivia/ViewModels/Game/AfterGameViewModel.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using ReactiveUI;
 using Trivia.Codec.S2C;
 using Trivia.Codec.S2C.Notification.Packets;
+using Trivia.Codec.S2C.Objects;
 using Trivia.Models;
 
 namespace Trivia.ViewModels.Game;
 
 public class AfterGameViewModel : SubRoomViewModel
 {
-    public AfterGameViewModel(IScreen hostScreen, RoomModel roomModel) : base(hostScreen, roomModel)
-    {}
+    public IList<PlayerResult> Results { get; }
+
+    public AfterGameViewModel(IScreen hostScreen, RoomModel roomModel, IList<PlayerResult> results) :
+        base(hostScreen, roomModel)
+    {
+        Results = results;
+    }
 
     public AfterGameViewModel()
-    {}
+    {
+        Results = [
+            // new PlayerResult()
+        ];
+    }
     
     
     protected override void CommOnPacketReceived(IS2CPacket packet)
