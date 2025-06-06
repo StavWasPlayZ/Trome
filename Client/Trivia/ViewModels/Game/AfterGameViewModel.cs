77e4ffb717372312cb6730221210433a6ff8e7a6
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-06 16:16:43 +0300

    Make After Game view adapt to all counts of attendees

--- Client/Trivia/ViewModels/Game/AfterGameViewModel.cs
+++ Client/Trivia/ViewModels/Game/AfterGameViewModel.cs
@@ -12,11 +12,13 @@ namespace Trivia.ViewModels.Game;
 
 public class AfterGameViewModel : SubRoomViewModel
 {
+    private const int MockUsers = 30;
+    
     public List<PlayerResultModel> Results { get; }
 
     public PlayerResultModel WinnerResults { get; private set; } = null!;
-    public List<PlayerResultModel> List2Results { get; private set; } = null!;
-    public List<PlayerResultModel> List3Results { get; private set; } = null!;
+    public List<PlayerResultModel>? List2Results { get; private set; }
+    public List<PlayerResultModel>? List3Results { get; private set; }
     
 
     public AfterGameViewModel(IScreen hostScreen, RoomModel roomModel, IList<PlayerResult> results) :
@@ -35,15 +37,15 @@ public class AfterGameViewModel : SubRoomViewModel
 
     public AfterGameViewModel()
     {
-        Results = Enumerable.Range(0, 30)
+        Results = Enumerable.Range(1, MockUsers)
             .Select(i => new User
             {
                 Id = i,
-                Username = $"User {i + 1}",
+                Username = $"User {i}",
             })
             .Select(user => new PlayerResultModel
             {
-                Place = user.Id + 1,
+                Place = user.Id,
                 
                 User = user,
                 PlaytimeSecs = 123,
@@ -62,11 +64,11 @@ public class AfterGameViewModel : SubRoomViewModel
         
         List2Results = Results.Count > 1
             ? Results[1..Math.Min(3, Results.Count)]
-            : [];
+            : null;
 
         List3Results = Results.Count > 3
             ? Results[3..]
-            : [];
+            : null;
     }
     
     
