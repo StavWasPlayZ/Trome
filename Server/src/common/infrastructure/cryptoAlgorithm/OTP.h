6f8216dc62867e93ec1ee5f3ce4b1c957b72dd60
Itay-He <hershkoitai@gmail.com>
2025-06-09 15:31:47 +0300

    Made OTP

---
+++ Server/src/common/infrastructure/cryptoAlgorithm/OTP.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CipherEncryption.h"
+#include <vector>
+
+class OTP : public ICryptoAlgorithm
+{
+public:
+	OTP();
+	std::string encrypt(const std::string &message) const override;
+	std::string decrypt(const std::string &message) const override;
+
+private:
+    static const std::vector<unsigned char> key;
+};
