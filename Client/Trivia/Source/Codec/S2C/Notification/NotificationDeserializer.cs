780ac48dc3858b6aa1430b88a0e75ab649a65dfd
Itay-He <hershkoitai@gmail.com>
2025-06-11 19:44:50 +0300

    Updated Deserializer

--- Client/Trivia/Source/Codec/S2C/Notification/NotificationDeserializer.cs
+++ Client/Trivia/Source/Codec/S2C/Notification/NotificationDeserializer.cs
@@ -15,6 +15,7 @@ public static class NotificationDeserializer
             NotificationCode.GameStarted => PacketDeserializer.Deserialize<GameStartedNotification>(json),
             NotificationCode.GameEnded => PacketDeserializer.Deserialize<GameEndedNotification>(json),
             NotificationCode.PlayerFinished => PacketDeserializer.Deserialize<PlayerFinishedNotification>(json),
+            NotificationCode.Kicked => PacketDeserializer.Deserialize<KickedNotification>(json),
             _ => null
         };
     }
