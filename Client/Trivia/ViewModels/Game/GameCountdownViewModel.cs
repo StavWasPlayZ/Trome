ed4182e443c9609ba60f7ba5e6f1cae1ddded367
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-12 01:55:43 +0300

    Make countdown functional

--- Client/Trivia/ViewModels/Game/GameCountdownViewModel.cs
+++ Client/Trivia/ViewModels/Game/GameCountdownViewModel.cs
@@ -1,12 +1,56 @@
+using System;
+using System.Reactive.Disposables;
+using Avalonia.Threading;
 using ReactiveUI;
 
 namespace Trivia.ViewModels.Game;
 
 public class GameCountdownViewModel : PageViewModel
 {
+    private DispatcherTimer? _countdownTimer;
+
     public GameCountdownViewModel(IScreen hostScreen) : base(hostScreen)
-    {}
+    {
+        _countdownTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        
+        this.WhenActivated(disposables =>
+        {
+            _countdownTimer.Tick += CountdownTimerOnTick;
+            _countdownTimer.Start();
+            
+            Disposable
+                .Create(() =>
+                {
+                    _countdownTimer.Stop();
+                    _countdownTimer.Tick -= CountdownTimerOnTick;
+                })
+                .DisposeWith(disposables);
+        });
+    }
 
     public GameCountdownViewModel()
     {}
+    
+    
+    private void CountdownTimerOnTick(object? sender, EventArgs e)
+    {
+        Countdown--;
+
+        if (Countdown == 0)
+        {
+            NavigateAndPop(new GameViewModel());
+        }
+    }
+
+    
+    private int _countdown = 5;
+
+    public int Countdown
+    {
+        get => _countdown;
+        set => this.RaiseAndSetIfChanged(ref _countdown, value);
+    }
 }
