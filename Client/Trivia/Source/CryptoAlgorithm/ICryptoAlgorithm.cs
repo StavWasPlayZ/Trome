cffe9b8e8118fbaf3fcdfe8f1455082225963684
Itay-He <hershkoitai@gmail.com>
2025-06-12 14:04:36 +0300

    Made file reader in client side

--- Client/Trivia/Source/CryptoAlgorithm/ICryptoAlgorithm.cs
+++ Client/Trivia/Source/CryptoAlgorithm/ICryptoAlgorithm.cs
@@ -1,7 +1,18 @@
-﻿namespace Trivia.CryptoAlgorithm;
+﻿using System.IO;
+
+namespace Trivia.CryptoAlgorithm;
 
 public interface ICryptoAlgorithm
 {
     string Encrypt(string message);
     string Decrypt(string message);
+    
+    protected static byte[] ReadFileBytes(string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException("The specified file was not found.", path);
+        }
+        return File.ReadAllBytes(path);
+    }
 }
