2760178ededaf94f452a78e3a5fc3ee8c15e470e
Itay-He <hershkoitai@gmail.com>
2025-06-12 14:49:18 +0300

    Made client side AES to use key files

--- Client/Trivia/Source/CryptoAlgorithm/AES.cs
+++ Client/Trivia/Source/CryptoAlgorithm/AES.cs
@@ -7,17 +7,11 @@ namespace Trivia.CryptoAlgorithm;
 
 public class AES : ICryptoAlgorithm
 {
-    private static readonly byte[] Key =
-    [
-        0x2A, 0x8D, 0xB4, 0x6C, 0xC1, 0xFA, 0x12, 0x34,
-        0x7B, 0x9E, 0x56, 0xAF, 0x44, 0xE1, 0x8C, 0x3D
-    ];
-
-    private static readonly byte[] IV = 
-    [
-        0x1F, 0x26, 0x73, 0x9A, 0x4B, 0xD5, 0xCE, 0x22,
-        0x39, 0x81, 0x67, 0xF4, 0x0C, 0xAB, 0x58, 0xE7
-    ];
+    private static readonly string KeyPath = "../../../Source/CryptoAlgorithm/Keys/AESkey.key";
+    private static readonly string IVPath = "../../../Source/CryptoAlgorithm/Keys/AESiv.key";
+    
+    private static readonly byte[] Key = ICryptoAlgorithm.ReadFileBytes(KeyPath);
+    private static readonly byte[] IV = ICryptoAlgorithm.ReadFileBytes(IVPath);
     
     public AES()
     {}
