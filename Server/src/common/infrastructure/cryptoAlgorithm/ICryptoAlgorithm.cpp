7d90e5d5ff7c65f9e121573fdeccb3fb4e5f5864
Itay-He <hershkoitai@gmail.com>
2025-06-12 13:42:03 +0300

    Made file opener static function

--- Server/src/common/infrastructure/cryptoAlgorithm/ICryptoAlgorithm.cpp
+++ Server/src/common/infrastructure/cryptoAlgorithm/ICryptoAlgorithm.cpp
@@ -1,4 +1,24 @@
 #include "ICryptoAlgorithm.h"
 
 ICryptoAlgorithm::ICryptoAlgorithm()
-{}
+{
+}
+
+std::vector<unsigned char> ReadFileBytes(const std::string &path)
+{
+    std::ifstream file(path, std::ios::binary);
+    if (!file)
+        return {}; // Return empty vector if file can't be opened
+
+    file.seekg(0, std::ios::end);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<unsigned char> buffer(size);
+    if (!file.read(reinterpret_cast<char *>(buffer.data()), size))
+    {
+        return {}; // Return empty vector if read fails
+    }
+
+    return buffer;
+}
--- Server/src/common/infrastructure/cryptoAlgorithm/ICryptoAlgorithm.h
+++ Server/src/common/infrastructure/cryptoAlgorithm/ICryptoAlgorithm.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <fstream>
 
 class ICryptoAlgorithm
 {
@@ -8,4 +10,7 @@ public:
     ICryptoAlgorithm();
     virtual std::string encrypt(const std::string &message) const = 0;
     virtual std::string decrypt(const std::string &message) const = 0;
+
+protected:
+    static std::vector<unsigned char> ReadFileBytes(const std::string &path);
 };
