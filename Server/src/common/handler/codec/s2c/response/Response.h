fa1df916b527ac696e04457e7f197f1c1a07e360
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-12 23:50:20 +0300

    Fix typos in deprecation messages

--- Server/src/common/handler/codec/s2c/response/Response.h
+++ Server/src/common/handler/codec/s2c/response/Response.h
@@ -134,7 +134,7 @@ struct LeaveRoomResponse : ProtocolResponse
 };
 
 struct [[deprecated(
-    "The Noftifications system has been set in place to allow for automatic, non-polling updates of any "
+    "The Notifications system has been set in place to allow for automatic, non-polling updates of any "
     "room state changes."
     " This request/response is therefore useless and should not be used."
 )]] GetRoomStateResponse : ProtocolResponse
@@ -190,7 +190,7 @@ struct SubmitAnswerResponse : QuestionResponse
 
 
 struct [[deprecated(
-    "The Noftifications system has been set in place to allow for automatic, non-polling updates of any "
+    "The Notifications system has been set in place to allow for automatic, non-polling updates of any "
     "room state changes."
     " This method is therefore useless and should not be used."
 )]] GetGameResultResponse : ProtocolResponse
