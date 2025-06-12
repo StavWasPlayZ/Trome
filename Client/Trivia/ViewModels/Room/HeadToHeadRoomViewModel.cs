ec381498443e574328e0d0cae19e21b7432ff5f7
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-12 18:22:51 +0300

    Add Start Game button in HeadToHeadRoomView

--- Client/Trivia/ViewModels/Room/HeadToHeadRoomViewModel.cs
+++ Client/Trivia/ViewModels/Room/HeadToHeadRoomViewModel.cs
@@ -1,28 +1,52 @@
-﻿using ReactiveUI;
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using ReactiveUI;
+using Trivia.Codec.C2S.Request.Packets;
 using Trivia.Codec.S2C;
 using Trivia.Codec.S2C.Notification.Packets;
+using Trivia.Codec.S2C.Response.Packets;
 using Trivia.Models;
 using Trivia.Models.User;
+using Trivia.ViewModels.Game;
 
 namespace Trivia.ViewModels.Room;
 
 public class HeadToHeadRoomViewModel : RoomViewModel
 {
+    public ReactiveCommand<Unit, Unit> StartGameCommand { get; }
+
     public HeadToHeadRoomViewModel(IScreen hostScreen, RoomModel roomModel) :
         base(hostScreen, roomModel, [])
     {
         _roomName = roomModel.Data.Name;
         MaxPlayers = 2;
+        
+        StartGameCommand = ReactiveCommand.CreateFromTask(
+            StartGame,
+            this.WhenAnyValue(x => x.Opponent)
+                .Select(opponent => opponent is not null)
+        );
 
         UpdatePlayerFields();
     }
 
     public HeadToHeadRoomViewModel()
     {
+        StartGameCommand = NoOpCommand;
+        
         _roomName = "ROOM NAME";
         
         Creator = HeadToHeadUserModel.FromUser(AppService.SessionUser!, 0);
         Opponent = null;
+    }
+    
+    
+    private async Task StartGame()
+    {
+        await Comm.SendRequestAwaitResponse<StartGameResponse>(new StartGameRequest(RoomModel.Data));
+
+        NavigateTo(new GameCountdownViewModel(HostScreen, RoomModel));
     }
 
 
