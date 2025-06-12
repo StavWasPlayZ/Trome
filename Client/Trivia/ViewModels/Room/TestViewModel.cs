d65e1cd197559e9003f4ce0940d657f5e8db33bd
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-12 13:04:24 +0300

    Design RoomTypeSelectorView

---
+++ Client/Trivia/ViewModels/Room/TestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ReactiveUI;
+
+namespace Trivia.ViewModels.Room;
+
+public class RoomTypeSelectorViewModel : PageViewModel
+{
+    public RoomTypeSelectorViewModel(IScreen hostScreen) : base(hostScreen)
+    {
+    }
+    
+    public RoomTypeSelectorViewModel() { }
+}
---
+++ Client/Trivia/Views/Room/RoomTypeSelectorView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Trivia.Views.Room;
+
+public partial class RoomTypeSelectorView : UserControl
+{
+    public RoomTypeSelectorView()
+    {
+        InitializeComponent();
+    }
+}
