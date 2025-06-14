96f96c75b37cfd0f5a16530b2fd1175c5295f38d
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-14 18:25:05 +0300

    Bind the Kick button of GuestStatsPopupView

--- Client/Trivia/ViewModels/Popups/GuestStatsPopupViewModel.cs
+++ Client/Trivia/ViewModels/Popups/GuestStatsPopupViewModel.cs
@@ -1,14 +1,28 @@
+using System;
+using System.Reactive;
+using ReactiveUI;
+using Trivia.Codec.C2S.Request.Packets;
+using Trivia.Codec.S2C.Response.Packets;
 using Trivia.Models.Raw;
 
 namespace Trivia.ViewModels.Popups;
 
 public class GuestStatsPopupViewModel : StatsPopupViewModel
 {
+    public ReactiveCommand<Unit, Unit> KickCommand { get; }
+    
     public GuestStatsPopupViewModel(User user) : base(user)
     {
+        KickCommand = ReactiveCommand.CreateFromTask(async _ =>
+        {
+            await Comm.SendRequestAsync<KickPlayerResponse>(new KickPlayerRequest(User.Id));
+            
+            CloseCommand.Execute().Subscribe();
+        });
     }
     
     public GuestStatsPopupViewModel()
     {
+        KickCommand = NoOpCommand;
     }
 }
