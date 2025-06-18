98f63f500ed707a9dbc425e42b37d445a3c577f1
StavWasPlayZ <stavsolomon101@gmail.com>
2025-06-18 03:10:31 +0300

    Fix signup validation only applying to SQLite

--- Server/src/common/infrastructure/db/IDatabase.cpp
+++ Server/src/common/infrastructure/db/IDatabase.cpp
@@ -13,30 +13,3 @@ float IDatabase::calcAverageAnswerTime(const int totalTime, const int totalAns)
 
     return static_cast<float>(totalTime) / totalAns;
 }
-
-void IDatabase::validateSignupInfo(
-    const std::string& password,
-    const std::string& email,
-    const std::string& phone,
-    const std::string& birthdate,
-    const std::optional<std::string>& address)
-{
-    if (!passMatcher.match(password))
-        throw RegexViolationException("password");
-
-    if (!emailMatcher.match(email))
-        throw RegexViolationException("email");
-
-    if (!phoneMatcher.match(phone))
-        throw RegexViolationException("phone");
-
-    if (!dateMatcher.match(birthdate))
-        throw RegexViolationException("birthdate");
-
-    if (address.has_value() && !addressMatcher.match(address.value()))
-        throw RegexViolationException("address");
-
-    //TODO: Actual date validation.
-    // Maybe.
-    // Perhaps not.
-}
